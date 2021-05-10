95c78ad8b76ab6bd8d3e9f33e0a4efe00f1ee5a9
Serhii Horoshko <sr.horoshko@gmail.com>
2021-05-10 19:03:37 +0300

    Minor, added modal logic for Cancel, Confirm and our promt click

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ function App() {
   return (
     <div>
       <h1>Demo APP</h1>
-      <AppList text='First App #001'/>
-      <AppList text='Second App #002'/>
-      <AppList text='Third App #003'/>
+      <AppList text="First App #001" />
+      <AppList text="Second App #002" />
+      <AppList text="Third App #003" />
     </div>
   );
 }
--- src/components/AppList.js
+++ src/components/AppList.js
@@ -1,15 +1,30 @@
+import { useState } from "react";
+import Backdrop from "./Backdrop";
+import Modal from "./Modal";
+
 function AppList(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
   function deleteHandler() {
-    console.log('Clickt');
-    console.log(props.text);
+    setModalIsOpen(true);
   }
-  
+
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+
   return (
     <div className="card">
-      <h2>{ props.text }</h2>
+      <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn" onClick={deleteHandler}>Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
+      {modalIsOpen && (
+        <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+      )}
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
     </div>
   );
 }
