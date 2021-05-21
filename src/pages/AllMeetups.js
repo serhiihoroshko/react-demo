1a5c0218841ec40510931b6412bfe66ebf9d983a
Serhii Horoshko <sr.horoshko@gmail.com>
2021-05-21 21:02:40 +0300

    Minor, All Meetups page was added

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,9 +1,11 @@
+import MeetupList from '../components/meetups/MeetupList';
+
 const DUMMY_DATA = [
   {
     id: 'm1',
     title: 'This is a first meetup',
     image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+      'https://news.itmo.ru/images/news/big/p8354.jpg',
     address: 'Meetupstreet 5, 12345 Meetup City',
     description:
       'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
@@ -12,7 +14,7 @@ const DUMMY_DATA = [
     id: 'm2',
     title: 'This is a second meetup',
     image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+      'https://news.itmo.ru/images/news/big/p8354.jpg',
     address: 'Meetupstreet 5, 12345 Meetup City',
     description:
       'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
@@ -23,11 +25,7 @@ function AllMeetupsPage() {
   return (
     <section>
       <h1>All Meetup</h1>
-      <ul>
-        {DUMMY_DATA.map((meetup) => {
-          return <li key={meetup.id}>{meetup.title}</li>;
-        })}
-      </ul>
+      <MeetupList meetups={DUMMY_DATA} />
     </section>
   );
 }
