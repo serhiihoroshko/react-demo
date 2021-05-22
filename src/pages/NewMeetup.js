410e7334da806b28ec4b332d016f6229784756b7
Serhii Horoshko <sr.horoshko@gmail.com>
2021-05-22 12:41:19 +0300

    Minor, AddePOST HTTP Request with sending to the Firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,20 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    fetch('https://react-demo-9567d-default-rtdb.firebaseio.com/meetups.json', {
+      method: 'POST',
+      body: JSON.stringify(meetupData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
